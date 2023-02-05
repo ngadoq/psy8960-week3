27ed99925ee5fb6511cdc4541668f18b29fa212b
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-02-05 00:12:04 -0600

    Line 12: Save the total time spent on the study by each participant in seconds into a new variable in clean_df called timeSpent using difftime(). You will probably want to look at the R documentation for difftime(). Line 13: Create a histogram of timeSpent. You will need to recast timeSpent as numeric first.

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,7 @@ raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- raw_df[!grepl("2017-06", raw_df$timeEnd),]
 clean_df <- clean_df[clean_df$q6 == 1,]
+
+# Analysis
+clean_df$timeSpent <- as.numeric(difftime(clean_df$timeEnd, clean_df$timeStart, units = "secs"))
+hist(clean_df$timeSpent)
