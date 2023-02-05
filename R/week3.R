0c8bf8200a0b37c2a58d65ba3a89620d88137b0a
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-02-05 00:06:39 -0600

    Q6 of this survey read “If you’re paying attention, answer Strongly Disagree (1) to this question.” Update clean_df so that it only contains people who were paying attention.

--- R/week3.R
+++ R/week3.R
@@ -6,3 +6,4 @@ raw_df <- read.csv(file = "../data/week3.csv")
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- raw_df[!grepl("2017-06", raw_df$timeEnd),]
+clean_df <- clean_df[clean_df$q6 == 1,]
