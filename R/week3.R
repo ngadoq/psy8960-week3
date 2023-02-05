cf62d6afe0c360fbdbc22b7cdded20fe5a2a8c1c
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-02-05 00:04:16 -0600

    Remove researchers

--- R/week3.R
+++ R/week3.R
@@ -5,3 +5,4 @@ getwd()
 raw_df <- read.csv(file = "../data/week3.csv")
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[!grepl("2017-06", raw_df$timeEnd),]
